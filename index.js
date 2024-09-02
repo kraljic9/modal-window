7277ecaa98ff4d2d862c30809c1a2c06f3d3e7fd
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-09-02 14:32:55 +0200

    added functionality to all parts and comments

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let closeBtn = document.querySelector(".close");
 let modal = document.querySelector(".modal");
 let overlay = document.querySelector(".overlay");
 
+console.log(showBtns);
+
+// Added eventListener to all buttons in the Node list items buttons in this case
 for (let i = 0; i < showBtns.length; i++) {
   showBtns[i].addEventListener("click", function () {
     modal.classList.remove("hidden");
@@ -12,12 +15,22 @@ for (let i = 0; i < showBtns.length; i++) {
   });
 }
 
+// Added eventListener to close button
 closeBtn.addEventListener("click", function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 });
 
+// Added eventListener to overlay
 overlay.addEventListener("click", function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+});
+
+// Added eventListener to document
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
 });
