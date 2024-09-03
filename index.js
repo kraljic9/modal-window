e13f1feedb21861a0d108f7d318bf4b4f7c76639
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-09-03 17:49:21 +0200

    Added new functions

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ let overlay = document.querySelector(".overlay");
 
 console.log(showBtns);
 
+// Function to show Modal
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Fucntion to remove Modal
+const removeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 // Added eventListener to all buttons in the Node list items buttons in this case
 for (let i = 0; i < showBtns.length; i++) {
   showBtns[i].addEventListener("click", function () {
